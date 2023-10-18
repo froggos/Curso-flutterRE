52dbf2df9e613673c73f57c2ca6300c4173d34ad
sapitolindo <benja.v.o@hotmail.com>
2023-10-17 22:07:56 -0300

    Dandole forma al asunto

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expenses/expenses.dart';
+import 'package:expenses/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- expenses/lib/widgets/expenses.dart
+++ expenses/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -28,11 +29,13 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text("El grafico"),
-          Text("Lista de gastos"),
+          const Text("El grafico"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
